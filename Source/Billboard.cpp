b3e69e81952e303f3f592bc617795fbb1f63f4ea
Robert Wen <robertwen20@gmail.com>
2015-08-09 01:38:36 -0400

    Added normal to the billboard

--- Source/Billboard.cpp
+++ Source/Billboard.cpp
@@ -103,7 +103,7 @@ void BillboardList::Update(float dt)
 	// @MYCODE
 	vec3 right = vec3(viewMatrix[0][0], viewMatrix[1][0], viewMatrix[2][0]);
 	vec3 up = vec3(viewMatrix[0][1], viewMatrix[1][1], viewMatrix[2][1]);
-    
+	vec3 lookAt = vec3(viewMatrix[0][2], viewMatrix[1][2], viewMatrix[2][2]);
     // @TODO 4 - Align billboards with Camera plane
     //
     // For each billboard, update each vertex position, color and normals
@@ -117,23 +117,29 @@ void BillboardList::Update(float dt)
 		// Top left
 		mVertexBuffer[firstVertexIndex].position = b->position + (0.5f * b->size.x * -right) + (0.5f * b->size.y * up);
 		mVertexBuffer[firstVertexIndex].color = b->color;
+		mVertexBuffer[firstVertexIndex].normal = -lookAt;
 		// Bottom Left
 		mVertexBuffer[firstVertexIndex + 1].position = b->position + (0.5f * b->size.x * -right) + (0.5f * b->size.y * -up);
 		mVertexBuffer[firstVertexIndex + 1].color = b->color;
+		mVertexBuffer[firstVertexIndex + 1].normal = -lookAt;
 		// Top Right
 		mVertexBuffer[firstVertexIndex + 2].position = b->position + (0.5f * b->size.x * right) + (0.5f * b->size.y * up);
 		mVertexBuffer[firstVertexIndex + 2].color = b->color;
+		mVertexBuffer[firstVertexIndex + 2].normal = -lookAt;
 
 		// Second Triangle
 		// Top Right
 		mVertexBuffer[firstVertexIndex + 3].position = b->position + (0.5f * b->size.x * right) + (0.5f * b->size.y * up);
 		mVertexBuffer[firstVertexIndex + 3].color = b->color;
+		mVertexBuffer[firstVertexIndex + 3].normal = -lookAt;
 		// Bottom Left
 		mVertexBuffer[firstVertexIndex + 4].position = b->position + (0.5f * b->size.x * -right) + (0.5f * b->size.y * -up);
 		mVertexBuffer[firstVertexIndex + 4].color = b->color;
+		mVertexBuffer[firstVertexIndex + 4].normal = -lookAt;
 		// Bottom Right
 		mVertexBuffer[firstVertexIndex + 5].position = b->position + (0.5f * b->size.x * right) + (0.5f * b->size.y * -up);
 		mVertexBuffer[firstVertexIndex + 5].color = b->color;
+		mVertexBuffer[firstVertexIndex + 5].normal = -lookAt;
         
         firstVertexIndex += 6;
     }
