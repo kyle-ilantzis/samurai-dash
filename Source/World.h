382d80f11c66adb7ebf9f0677d99f01c34bd32f2
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-08-11 22:51:53 -0400

    demo with capsules and short spline

--- Source/World.h
+++ Source/World.h
@@ -39,7 +39,7 @@ class World
 public:
 	static const char* sceneFile;
 	// If true then models with bounding volumes will draw the volumes
-	static const bool DRAW_BOUNDING_VOLUME = false;
+	static const bool DRAW_BOUNDING_VOLUME = true;
 	// If true the animations will draw their key frames points as a path
 	static const bool DRAW_ANIM_PATH = false;
 	// How long to wait after the player has died or reached the goal before restarting.
